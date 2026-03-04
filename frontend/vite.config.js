3030dccb04b8812193f8117b9b872112a840c524
A. Lechev <aleksandarlechev26@itpg-varna.bg>
2026-03-04 15:31:54 +0200

    Fix API env changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+const API_URL = import.meta.env.VITE_API_URL;
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   server: {
     proxy: {
-      "/fetch-image": "http://localhost:8000"
+      "/fetch-image": API_URL
     }
   }
 })
