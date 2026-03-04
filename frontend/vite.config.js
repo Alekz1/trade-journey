c927c08561600e05c79413fe6de37be4a61c5748
A. Lechev <aleksandarlechev26@itpg-varna.bg>
2026-03-04 15:38:27 +0200

    Rewrote vite config to work with the env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-const API_URL = import.meta.env.VITE_API_URL;
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {
-    proxy: {
-      "/fetch-image": API_URL
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  const API_URL = env.VITE_API_URL;
+
+  return defineConfig({
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/fetch-image": API_URL
+      }
     }
-  }
-})
+  });
+};
